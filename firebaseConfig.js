3e8049a312f0baef11cf0e727e9889e41b4bc702
notrlysam <sameer.xavier2@gmail.com>
2026-04-11 20:40:20 -0400

    Adjusted file pathing for correct github repository file upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAuLiO0hwj6P6NmIwSGQ9VuwIt_4sUzDII",
+  authDomain: "kns-cocktailfinder.firebaseapp.com",
+  projectId: "kns-cocktailfinder",
+  storageBucket: "kns-cocktailfinder.firebasestorage.app",
+  messagingSenderId: "555187883337",
+  appId: "1:555187883337:web:a599a5c52d72f70b419e98",
+};
+
+export default firebaseConfig;
